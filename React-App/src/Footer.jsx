e9484998d5dccd1cbcb145a1c40122300c256d0e
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-09-13 09:24:08 +0300

    Refactor App component structure and remove unused styles. Introduced Header, Footer, and Food components, while cleaning up App.css and index.css for improved layout and styling.

---
+++ React-App/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <>
+            <h3>all rights reserved {new Date().getFullYear() }</h3>
+        </>
+    );
+}
+
+export default Footer;
---
+++ React-App/src/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <>
+            <h2>Hello Everyone, I am Ahmed Albahrawy</h2>
+            <h3>And I am going to learn React with Vite</h3>
+        </>
+    );
+}
+
+export default Header;
