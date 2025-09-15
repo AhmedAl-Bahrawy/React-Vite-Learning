8a7ab8bb2175f19f48bac3768373572415d2fc0b
Ahmed Al-Bahrawy <ahmedalbahrawy-2010@hotmail.com>
2025-09-15 07:01:05 +0300

    Enhance ProfilePicture component by modifying handleClick2 to be asynchronous, allowing for a delay before the image is displayed again after being clicked. This improves user interaction by providing a visual feedback delay.

--- React-App/src/components/ProfilePicture/ProfilePicture.jsx
+++ React-App/src/components/ProfilePicture/ProfilePicture.jsx
@@ -4,9 +4,11 @@ function ProfilePicture() {
     const handleClick = () => {
         console.log("Image Clicked");
     }
-    const handleClick2 = (e) => { 
+    const handleClick2 = async (e) => { 
         e.target.style.display = 'none';
         console.log("Image Clicked");
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        e.target.style.display = 'block';
     };
     return (
         <img src={imageURL} alt="Random Image" onClick={(e) => handleClick2(e)}/>
